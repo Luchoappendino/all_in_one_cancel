c2bbb58d2ad59d4e2249e5cb366c79b1e7387794
https://Anjeel@bitbucket.org <https://Anjeel@bitbucket.org>
2020-03-23 19:22:55 +0530

    Commit

--- models/all_in_one_cancel.py
+++ models/all_in_one_cancel.py
@@ -20,6 +20,14 @@ class PurchaseOrderCancel(models.Model):
     _inherit = 'purchase.order'
 
     def button_cancel(self):
+        pick = self.env['stock.picking'].search([('origin', '=', self.name)])
+        pick.move_line_ids_without_package.state = 'cancel'
+        pick.move_line_ids_without_package.qty_done = 0
+        pick.write({'state': 'cancel'})
+        pick.unlink()
+        account = self.env['account.move'].search([('invoice_line_ids.name', 'like', self.name)])
+        account.write({'state': 'cancel'})
+        account.unlink()
         return self.write({'state': 'cancel'})
 
 
@@ -27,10 +35,36 @@ class AccountCancel(models.Model):
     _inherit = 'account.move'
 
     def button_cancel(self):
+        self.unlink()
         return self.write({'state': 'cancel'})
 
     def unlink(self):
         for move in self:
             move.line_ids.unlink()
+
+
+class ResPartnerInherit(models.Model):
+    _inherit = 'res.partner'
+
+    def _compute_supplier_invoice_count(self):
+        # retrieve all children partners and prefetch 'parent_id' on them
+        all_partners = self.search([('id', 'child_of', self.ids)])
+        all_partners.read(['parent_id'])
+
+        supplier_invoice_groups = self.env['account.move'].read_group(
+            domain=[('partner_id', 'in', all_partners.ids),
+                    ('type', 'in', ('in_invoice', 'in_refund')),
+                    ('state', '!=', 'cancel')],
+            fields=['partner_id'], groupby=['partner_id']
+        )
+        partners = self.browse()
+        for group in supplier_invoice_groups:
+            partner = self.browse(group['partner_id'][0])
+            while partner:
+                if partner in self:
+                    partner.supplier_invoice_count += group['partner_id_count']
+                    partners |= partner
+                partner = partner.parent_id
+        (self - partners).supplier_invoice_count = 0
 
 
